f9ec4c73539d8f157852cc2e9e5ba9a8f63c5885
Davide Peterlini <davide.peterlini@marconirovereto.it>
2025-01-19 16:31:16 +0100

    setup ionic

---
+++ android/app/src/main/java/ionic/pankey/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.pankey;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.pankey',
+  appName: 'pankey',
+  webDir: 'dist'
+};
+
+export default config;
